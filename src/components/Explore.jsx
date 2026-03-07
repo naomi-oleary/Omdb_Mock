6958b04812b9d296a51e6da3587d1d5a5a2d4129
naomi-oleary <oleary.ng@gmail.com>
2026-03-06 17:32:55 -0700

    working on carousel

--- src/components/Explore.jsx
+++ src/components/Explore.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Logo from '../assets/Logo.jpg';
 
 export default function Explore() {
   return (
@@ -9,7 +10,20 @@ export default function Explore() {
                     Explore <span className="gold">top-rated movies</span> and ace your movie night
                 </h2>
                 <div className="carousel__container">
-                    <div className="carousel__item"></div>
+                    <div className="carousel__items">
+                        <div className="carousel__slide">
+                            1
+                        </div>
+                        <div className="carousel__slide">
+                            2
+                        </div>
+                        <div className="carousel__slide">
+                            3
+                        </div>
+                        <div className="carousel__slide">
+                            4
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -8,6 +8,7 @@ const Landing = () => {
         <div className="landing__row">
           <h1 className="gold">RIMBERIO</h1>
           <h2>Browse Your Favorite Films</h2>
+          <input type="text" className="search-bar" />
           <div className="landing__img--container">
             <img src={ MovieImg } className="landing--img" />
           </div>
